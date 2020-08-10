370d4f511fccbea5df4ed899db7794ba4d23ecb5
karan obhrai <mrkobhrai@gmail.com>
2020-08-10 17:21:14 +0100

    Update FAQ for consistency

--- frontend/src/components/Pages/HomePage.jsx
+++ frontend/src/components/Pages/HomePage.jsx
@@ -56,14 +56,12 @@ class HomePage extends Component {
                   <h5><b>How does it all work?</b></h5>
                 </CardTitle>
                 <CardText>
-                  Parents are existing 2nd, 3rd, and 4th year Computing and JMC
-                  students in DoC. Each family will consist of two parents, and
-                  up to 6 freshers. After term commences, there will be a
-                  timetabled slot for freshers to meet their families, along
-                  with some food and activities to get to know each other. Each
-                  term there is also a small budget for each family (around 20
-                  pounds) to use as appropriate (dinner, drinks, any family
-                  gathering etc).
+                Parents are existing 2nd, 3rd, and 4th year Computing and JMC students
+                in DoC. Each family will consist of two parents and up to five freshers.
+                This year, due to the unusual circumstances, family activities 
+                in the Autumn term will be held remotely. There will be a range of
+                opportunities for you to bond with your family, starting with a virtual pub quiz.
+                Each term there is also a small budget for each family (around 20 pounds) to use as appropriate.
                 </CardText>
                 <CardTitle>
                   <h5><b>Can I sign up as a solo parent?</b></h5>
@@ -73,18 +71,6 @@ class HomePage extends Component {
                   partner - this is done to ensure the best possible experience
                   for incoming freshers. If you sign up without choosing a
                   partner, your registration will be ignored.
-                </CardText>
-                <CardTitle>
-                  <h5>
-                    <b>
-                      I've already signed up on the union website. Do I sign up
-                      here too?
-                    </b>
-                  </h5>
-                </CardTitle>
-                <CardText>
-                  Yes, please do. If you haven't signed up on the union website
-                  then you won't need to, as long as you sign up here.
                 </CardText>
                 <CardTitle>
                   <h5><b>How will freshers be allocated?</b></h5>
